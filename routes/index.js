92813a4f18b157d3a1132c0edb79ba1e8205650e
Juan Camilo <juanccamachob94@gmail.com>
2022-09-08 22:11:00 -0500

    controllers architecture added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+const router = require('express').Router();
+const IndexController = require('../controllers/IndexController');
+router.route('/')
+  .get(IndexController.index)
+  ;
 module.exports = router;
